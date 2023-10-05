72d0732c4ee7ea61cf44f1df7774583b502510c6
timmySpark <timmyspark1@gmail.com>
2023-10-05 09:12:08 -0700

    [feat]:- prints the number and lists of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+
+    import sys
+    
+    args = sys.argv[1:]
+    no_of_args = len(args)
+
+    if no_of_args == 0:
+        print(f'{no_of_args} arguments.')
+    elif no_of_args == 1:
+        print(f'{no_of_args} argument:')
+    else:
+        print(f'{no_of_args} arguments:')
+
+    for count, arg in enumerate(args, start=1):
+        print(f'{count}: {arg}')
