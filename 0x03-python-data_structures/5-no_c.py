4bd8f5ae63b9f381242bfb82901da37b1587be0f
timmySpark <timmyspark1@gmail.com>
2023-10-07 15:08:31 -0700

    [feat] :- removes all characters c and C from string sent

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_strings = []
+
+    for str in my_strings:
+        if str != 'c' and str != 'C':
+            new_strings.append(str)
+    return "".join(newstrings)
