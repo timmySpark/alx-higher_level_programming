8abc056d888aa296bf2af52a1882e62ed6619378
timmySpark <timmyspark1@gmail.com>
2023-10-08 15:56:46 -0700

    [feat] :- find the largest integer from the list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max = my_list[0]
+
+    if my_list == "":
+        return None
+    for i in range(len(my_list)):
+        if my_list[i] > max:
+            max = my_list[i]
+    return max
