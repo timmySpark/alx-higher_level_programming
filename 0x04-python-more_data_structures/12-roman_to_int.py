42e477e2ab33d21b52309f72e8502a4061ead5bd
timmySpark <timmyspark1@gmail.com>
2023-10-11 14:18:13 -0700

    [feat] :- solving issues gradual;y

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!usr/bin/python3
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
@@ -19,13 +19,12 @@ def roman_to_int(roman_string):
 
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
-            return 0
+            return (0)
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
+                num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
-
-        return num
+    return (num)
