95227d47b5211d7d0d6ac98895b7ad314b596290
timmySpark <timmyspark1@gmail.com>
2023-10-11 14:07:43 -0700

    [feat] :- worked on pylist objects and cpython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,14 +1,10 @@
-#!/usr/bin/python3
-
 #include <Python.h>
-
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
 
 /**
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
  */
+
 void print_python_list(PyObject *p)
 {
 	int size, alloc, i;
@@ -66,3 +62,4 @@ void print_python_bytes(PyObject *p)
 			printf(" ");
 	}
 }
+
