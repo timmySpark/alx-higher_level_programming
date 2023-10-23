d5e5f790a6c2c28fdb0edc09d4eb3f43f50fe0a6
timmySpark <timmyspark1@gmail.com>
2023-10-23 11:25:42 -0700

    [feat] :- removed extra spacing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,15 +5,13 @@ def safe_print_list(my_list=[], x=0):
     prints X elements of a list
 
     Args:
-        My_list : The list to prints elements from
-        x : Numbers of elements of my_list to be printed
+        My_list (list) : The list to prints elements from
+        x (int) : Numbers of elements of my_list to be printed
 
     Return:
         Numbers of element printed
     """
-
     count = 0
-
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
