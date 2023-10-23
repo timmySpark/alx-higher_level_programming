025f6bd260d76df93aa3e99509bdbfa01aefd5c5
timmySpark <timmyspark1@gmail.com>
2023-10-23 11:50:45 -0700

    [feat] :- getting the right way to write a comment

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """
-    prints X elements of a list
+    """prints X elements of a list
 
     Args:
         My_list (list) : The list to prints elements from
         x (int) : Numbers of elements of my_list to be printed
 
-    Return:
+    Returns:
         Numbers of element printed
     """
     count = 0
