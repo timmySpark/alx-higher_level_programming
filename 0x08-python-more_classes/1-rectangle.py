7619f4d0b82c2f72eb726a6b1764315d2071f7c3
timmySpark <timmyspark1@gmail.com>
2023-10-30 17:42:30 -0700

    [feat] :- working on passing requirements

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        """
+        """ Initialize a new Rectangle
             Args:
                 width (int)  :- width of a rectangle
                 height (int) :- height of the rectangle
@@ -21,12 +21,12 @@ class Rectangle:
 
         @property
         def width(self):
-            """ Get/Set width of rectangle"""
+            """ Get/Set width of rectangle """
             return self.__width
 
         @width.setter
         def width(self, value):
-            if not(isinstance(value, int)):
+            if not isinstance(value, int):
                 raise TypeError("width must be an integer")
             if value < 0:
                 raise ValueError("width must be >= 0")
@@ -39,7 +39,7 @@ class Rectangle:
 
         @height.setter
         def height(self, value):
-            if not(isinstance(value, int)):
+            if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
