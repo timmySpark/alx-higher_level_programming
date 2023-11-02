db45b13bcdcf6ba593fdfa75796a7adf40a82621
timmySpark <timmyspark1@gmail.com>
2023-11-02 15:01:16 -0700

    [feat] :- documented the functions and saymyname midule

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" say_my_name module"""
+
 
 def say_my_name(first_name, last_name=""):
+    """ prints myname is <first_name> <last_name>"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
