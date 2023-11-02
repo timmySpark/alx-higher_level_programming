f21ad50858fcb02fd3f95f39928328ef3ecb8ed6
timmySpark <timmyspark1@gmail.com>
2023-11-02 15:34:38 -0700

    [feat] :- documenting module and functions

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" module defining text_indentation"""
+
 
 def text_indentation(text):
+    """ prints a text with 2 new lines after each of these characters:
+        ., ? and :
+    """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     text = text.strip()
