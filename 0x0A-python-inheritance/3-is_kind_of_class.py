0fcd2e60e990e1a26f88e94d2e1fc65fa6c99651
timmySpark <timmyspark1@gmail.com>
2023-11-06 15:07:33 -0800

    [feat] :- checks if an obj is an instance or if the obj is an instance of a class inherited from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """ checks if object is an instance of or
+    is an instance of a class inherited from the specified class
+
+    Args:
+        obj (type) :- object to be checked
+        a_class (type) :- the class to be matched with obj
+
+    Returns:
+    if Object is an instance of or instance of a class - True
+    Otherwise - False
+
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
