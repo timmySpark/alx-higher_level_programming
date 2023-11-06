d2bcf00c240ae61e5fed85d1435dda8bef508694
timmySpark <timmyspark1@gmail.com>
2023-11-06 14:56:21 -0800

    [feat] :- still working on task2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""Defines a class-checking function."""
+
 
 def is_same_class(obj, a_class):
-    """checks if an onbect is exactly an instance of a specified class
+    """Check if an object is exactly an instance of a given class.
 
     Args:
-        obj (any): Object to be checked
-        a_class (type) : The Class to be matched the type of obj to
-
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        If Obj is exactly an instance of a_class - True
-        Otherwise - False
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
     """
     if type(obj) == a_class:
         return True
