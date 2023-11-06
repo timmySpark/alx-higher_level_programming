d3fb8804b3b9a88c936ffc4537cd50ae2e5f7e5c
timmySpark <timmyspark1@gmail.com>
2023-11-06 15:45:58 -0800

    [feat] :- validates value and check if it's an int or less than and equal to 0

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Defines a BaseGeometry Class  """
+
+
+class BaseGeometry():
+    """ Represents BaseGEomtry Class"""
+
+    def area(self):
+        """ raises an exception with area not implemented message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
