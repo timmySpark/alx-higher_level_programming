f671d88bada3a8e4b3ff0672610f47a2cf249ba3
timmySpark <timmyspark1@gmail.com>
2023-11-07 16:50:26 -0800

    [feat] :- appending a text of strings to a  file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,11 @@
 
 def append_write(filename="", text=""):
     """ appends a sring to the end of a file
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
     with open(filename, 'a', encoding="utf-8") as fd:
         return fd.write(text)
