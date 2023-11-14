e55d8494ab2ad483b05942c17a703eabcf0f98f6
timmySpark <timmyspark1@gmail.com>
2023-11-13 16:07:56 -0800

    [feat] :- removed extra space in error message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,7 +56,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be >=  0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
