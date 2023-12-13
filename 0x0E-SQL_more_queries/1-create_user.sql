836e54628a4eb12f78156505887efa4dd95182f1
timmySpark <timmyspark1@gmail.com>
2023-12-13 14:58:27 -0800

    [feat] :- added host to user , user@localhost

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create a User user_0d_1 with all privileges.
 CREATE USER 
-	IF NOT EXISTS user_0d_1
+	IF NOT EXISTS user_0d_1@localhost
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1@localhost; 
