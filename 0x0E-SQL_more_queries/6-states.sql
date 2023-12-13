55c72a60d07a1cbf437689086dff732a45e54a46
timmySpark <timmyspark1@gmail.com>
2023-12-13 15:18:51 -0800

    [feat] :- created a database hbtn_0d_usa with a table states, primary key attached to id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create a Database hbtn_0d_usa and a table states.
+
+CREATE DATABASE
+	IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+	);
