b119133bdb368401b8ea669974e58674fdce00b7
timmySpark <timmyspark1@gmail.com>
2023-12-13 17:38:12 -0800

    [fix] :- trying to find requirement fail

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,4 @@
--- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-
+-- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
 SELECT g.name
   FROM tv_genres AS g
         INNER JOIN tv_show_genres AS s
