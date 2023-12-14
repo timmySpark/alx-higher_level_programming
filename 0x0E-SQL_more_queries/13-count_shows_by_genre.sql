ff018ca28585f3947f4d335a567ce767b7ae2f46
timmySpark <timmyspark1@gmail.com>
2023-12-13 16:25:22 -0800

    [fix] :- solving little typo in join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,6 @@ SELECT m.name genre,
 	COUNT(*) number_of_shows
   FROM tv_genres AS m
         INNER JOIN tv_show_genres AS g
-	ON m.id = g.show_id
+	ON m.id = g.genre_id
   GROUP BY m.name
   ORDER BY number_of_shows DESC;
