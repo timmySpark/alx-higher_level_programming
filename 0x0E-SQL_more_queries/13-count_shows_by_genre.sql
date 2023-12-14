dfd4afc768c7e64d60ccb22148286bbbab306cb2
timmySpark <timmyspark1@gmail.com>
2023-12-13 16:19:31 -0800

    [feat] :- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have 
+-- 						at least one genre linked.
+
+SELECT m.name genre, 
+	COUNT(*) number_of_shows
+  FROM tv_genres AS m
+        INNER JOIN tv_show_genres AS g
+	ON m.id = g.show_id
+  GROUP BY m.name
+  ORDER BY number_of_shows DESC;
