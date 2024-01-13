5eea608c736c67e51e94fcc17f63a8d3fa176574
timmySpark <timmyspark1@gmail.com>
2024-01-13 14:04:51 -0800

    [feat] :- lists all states from the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# List all the sataes from database hbtn_0e_0_usa
+#  Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
+from sys import argv
+import MySQLdb
+
+conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+curr = conn.cursor()
+curr.execute("select * FROM states")
+[print(state) for state in curr.fetchall()]
+curr.close()
+conn.close()
