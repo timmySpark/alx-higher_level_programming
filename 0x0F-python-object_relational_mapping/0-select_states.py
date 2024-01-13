e3e70a4b363f78cec1a3e3af4b4f348a4a0ee37e
timmySpark <timmyspark1@gmail.com>
2024-01-13 14:13:04 -0800

    [feat] :- trying to pass a requirement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,10 +13,12 @@ if __name__ == "__main__":
             port=3306,
             user=argv[1],
             passwd=argv[2],
-            db=argv[3]
+            db=argv[3],
+            charset='utf8'
         )
+
     curr = conn.cursor()
-    curr.execute("select * FROM states")
+    curr.execute("SELECT * FROM states ORDER BY id")
     [print(state) for state in curr.fetchall()]
     curr.close()
     conn.close()
