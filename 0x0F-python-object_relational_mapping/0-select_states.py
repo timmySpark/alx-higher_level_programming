a7fac86f1b69f3606ab450a4fd3c006ff697c15e
timmySpark <timmyspark1@gmail.com>
2024-01-13 16:44:35 -0800

    [feat] :- trying to pass a requirement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-# List all the sataes from database hbtn_0e_0_usa
-#  Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+'''Script that lists all states from the database hbtn_0e_0_usa'''
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     conn = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset='utf8'
-        )
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
 
     curr = conn.cursor()
-    query = "SELECT * FROM states ORDER BY id"
+    query = "SELECT * FROM states ORDER BY id ASC"
     curr.execute(query)
 
     for row in curr.fetchall():
