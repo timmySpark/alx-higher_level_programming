4d59b290613b428dc64f32aeadde712a451714cb
timmySpark <timmyspark1@gmail.com>
2024-01-13 21:45:02 -0800

    [feat] :- adding an else statement, make more checks

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
-        session.close()
+    if not state:
+        print("Nothing")    
+    print("{}: {}".format(state.id, state.name))
+    session.close()
