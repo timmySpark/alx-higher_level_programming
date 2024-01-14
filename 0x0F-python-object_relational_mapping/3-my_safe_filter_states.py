521b7947b82f97a58ed1f7e9eef63812728631eb
timmySpark <timmyspark1@gmail.com>
2024-01-13 17:39:35 -0800

    [feat] :- using sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+
+    curr = conn.cursor()
+    query = '''
+        SELECT * FROM states
+        ORDER BY id ASC
+        '''
+    curr.execute(query)
+    [print(state) for state in curr.fetchall() if state[1] == argv[4]]
