b2e3aadcc18cd53c37af8280c3b7207abeee1580
timmySpark <timmyspark1@gmail.com>
2024-01-13 16:54:11 -0800

    [feat] :- lists all states starting eith the letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Script that lists all states from the database hbtn_0e_0_usa'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+
+    curr = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+    curr.execute(query)
+    [print(state) for state in curr.fetchall()]
+    curr.close()
+    conn.close()
