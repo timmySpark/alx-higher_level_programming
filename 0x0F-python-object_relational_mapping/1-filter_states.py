da1ac8fcee12f11f81c1a3819c241ccb4f51db6c
timmySpark <timmyspark1@gmail.com>
2024-01-14 17:45:26 -0800

    [feat] :- working on check errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
 '''
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
+    Lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
 '''
 
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    conn = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset='utf8'
-    )
-
-    curr = conn.cursor()
-    query = '''
-        SELECT * FROM states
-        WHERE name LIKE 'N%'
-        ORDER BY id ASC
-        '''
-    curr.execute(query)
-
-    for row in curr.fetchall():
-        print(row)
-
-    curr.close()
-    conn.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
