05702c7a125707aa8f9c3b9b8a4daaabb584f114
timmySpark <timmyspark1@gmail.com>
2024-01-14 17:47:42 -0800

    [feat] :- working on check errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,28 +4,13 @@
     in the states table of hbtn_0e_0_usa where name matches the argument
 '''
 
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    conn = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset='utf8'
-    )
-
-    curr = conn.cursor()
-    query = '''
-        SELECT * FROM states
-        WHERE name='{}'
-        ORDER BY id ASC
-        '''.format(argv[4])
-    curr.execute(query)
-
-    [print(state) for state in curr.fetchall()]
-
-    curr.close()
-    conn.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
