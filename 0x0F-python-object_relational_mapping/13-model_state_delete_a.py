7027b7d90abbe70c6599f17ece73b6b2f12b5779
timmySpark <timmyspark1@gmail.com>
2024-01-14 17:50:33 -0800

    [feat] :- working on check errors

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 '''  a script that lists all State objects from the database hbtn_0e_6_usa '''
 
-from sys import argv
+import sys
 import sqlalchemy
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
