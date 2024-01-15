fa8d5888b841fd385223ae29d5a4b9ab81cf053d
timmySpark <timmyspark1@gmail.com>
2024-01-14 17:43:00 -0800

    [feat] :- working on check errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
-    Script that lists all states from the database hbtn_0e_0_usa
-    where name starts from N(Uppercase)
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 '''
 
 import MySQLdb
