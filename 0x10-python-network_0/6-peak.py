4e4ca8d3859158469d0cd3f143e8dcfd3fd1001b
timmySpark <timmyspark1@gmail.com>
2024-01-25 18:52:06 -0800

    [feat] :- finding the picking in a list of integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""contains the function find_peak"""
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
