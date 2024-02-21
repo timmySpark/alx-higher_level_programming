4a1b46dfd46a7a9b82311f4dbaf22fef6b661fae
timmySpark <timmyspark1@gmail.com>
2024-02-20 17:47:03 -0800

    [feat] :- displays the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const request = require('request');
-filename = process.argv[2];
-request.get(filename).on('response', (response) =>
+request.get(process.argv[2]).on('response', (response) =>
   console.log(`code: ${response.statusCode}`));
