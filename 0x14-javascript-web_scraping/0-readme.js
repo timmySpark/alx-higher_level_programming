e0c33cba88f3f578da7c48b46ed352fb975ca834
timmySpark <timmyspark1@gmail.com>
2024-02-20 17:29:57 -0800

    [feat] :- script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
